0fd52cb1ba7cdfcf79c0e0ec1ea6d4a6e1726b61
ryanthornton628 <ryanthornton628@gmail.com>
2019-05-06 18:53:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+const n = Math.floor(Math.random() * 1000)
+animal = 'dog';
+
+
+
+function myAnimal() {
+  var animal = 'dog'
+  return animal
+}
+
+function yourAnimal() {
+  var animal = 'cat'
+  return animal
+}
+
+
+function add2(n) {
+const two = 2
+ return n + two
+}
+
+function funkyFunction() {
+  return function() { 
+    return "FUNKY!"
+  }
+}
+var theFunk = funkyFunction()()
